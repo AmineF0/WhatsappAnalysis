043f8a0c0c1ffcaac351910511845d2d7825f911
AmineF0 <aminefirdawsi@gmail.com>
2022-05-11 21:38:12 +0100

    upload

--- p.py
+++
@@ -1,9 +0,0 @@
-#importing modules
-import pytesseract
-from PIL import Image
-
-# If you don't have tesseract executable in your PATH, include the following:
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-
-#converting image to text
-print(pytesseract.image_to_string(Image.open(r'C:\Users\Amine\Pictures\Screenshots\Screenshot (14).png')))
